10917f3ec8b8ea040ba95841274603155480b91c
Ritchie Caruso <ritchiecaruso7@gmail.com>
2024-03-15 20:48:56 +1100

    add function to change grid size

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-let sketcher = document.querySelector('.sketcher');
-sketcher.style.gridTemplateColumns = 'repeat(16, 1fr)';
-sketcher.style.gridTemplateRows = 'repeat(16, 1fr)';
-for(let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'green'
-    sketcher.insertAdjacentElement('beforeend', square);
+function createSketcher(size) {
+    let sketcher = document.querySelector('.sketcher');
+    let squares = sketcher.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
+    sketcher.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketcher.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'green'
+        sketcher.insertAdjacentElement('beforeend', square);
 
+    }
+}
+
+createSketcher(16);
+
+function changeGridSize(input) {
+    if(input >= 2 && input <= 100) {
+        createSketcher(input);
+    } else {
+        alert("Enter a number between 2 and 100.")
+    }
+    
 }
